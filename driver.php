4654d6a05d70b8a694b77bde883749bdda30c953
jzheadley <headleyjz@vcu.edu>
2018-10-23 13:05:33 -0400

    made the driver even more generic so I can just call a method to generate data

--- driver.php
+++ driver.php
@@ -2,29 +2,39 @@
 include "really-simple-captcha.php";
 $numChars = 7;
 $charWidth = 64;
-$fontSize=$charWidth;
-$numImages=5;
-$imageHeight=100;
-$imageWidth=($numChars + 1) * $charWidth;
-$trainDir="train";
-
+$fontSize = $charWidth;
+$numTrain = 1000;
+$numTest = 1000;
+$imageHeight = 100;
+$trainDir = "train";
+$testDir = "test";
 
 // create the instance of the generator
-$captcha_instance = new ReallySimpleCaptcha();
 // set the generator variables to be what we want
-$captcha_instance->tmp_dir = __DIR__ . '/' . $trainDir;
+function generate_dataset($dir, $numChars, $numImages)
+{
+    echo ("Generating images in " . $dir . "\n");
+    $captcha_instance = new ReallySimpleCaptcha();
 
-$captcha_instance->char_length = $numChars;
-$captcha_instance->font_char_width = $charWidth;
-$captcha_instance->img_size = array($imageWidth, $imageHeight);
-$captcha_instance->font_size=$fontSize;
-$captcha_instance->base=array((1 /12) * $imageWidth, $imageHeight*.75);
+    $imageWidth = ($numChars + 1) * $GLOBALS['charWidth'];
 
-for ($i = 0; $i < $numImages; $i++) {
-    // generate a random word
-    $word = $captcha_instance->generate_random_word();
+    $captcha_instance->tmp_dir = __DIR__ . '/' . $dir;
+    $imageWidth = ($numChars + 1) * $GLOBALS['charWidth'];
+    $captcha_instance->char_length = $numChars;
+    $captcha_instance->font_char_width = $GLOBALS['charWidth'];
+    $captcha_instance->img_size = array($imageWidth, $GLOBALS['imageHeight']);
+    $captcha_instance->font_size = $GLOBALS['fontSize'];
+    $captcha_instance->base = array((1 / 12) * $imageWidth, $GLOBALS['imageHeight'] * .75);
 
-    echo ($word . "\n");
-    // generate the image file and write it to filesystem
-    $captcha_instance->generate_image($word, $word);
+    for ($i = 0; $i < $numImages; $i++) {
+        // generate a random word
+        $word = $captcha_instance->generate_random_word();
+
+        echo ($word . "\n");
+        // generate the image file and write it to filesystem
+        $captcha_instance->generate_image($word, $word);
+    }
 }
+
+generate_dataset($trainDir, 4, $numTrain);
+generate_dataset($testDir, 7, $numTest);
