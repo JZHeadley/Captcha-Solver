261ee9a6c87a5ea9d0a9c854fba1a667ed3b5b92
jzheadley <headleyjz@vcu.edu>
2018-10-24 17:10:48 -0400

    can successfully get captchas out of amazon

--- getting_captchas/bullying-amazon/forcing_amazon_captchas.py
+++ getting_captchas/bullying-amazon/forcing_amazon_captchas.py
@@ -1,19 +1,26 @@
 import requests
-from bs4 import BeautifulSoup
-import mechanicalsoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+import urllib
+
+numCaptchas=100
 
 if __name__ == '__main__':
-    sign_in_link="https://www.amazon.com/ap/signin?openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&&openid.pape.max_auth_age=0"
-    sign_in_page=requests.get(sign_in_link)
-    sign_in_page_soup = BeautifulSoup(sign_in_page.content, 'html.parser')
-    # print(sign_in_page_soup)
-    browser=mechanicalsoup.StatefulBrowser()
-    browser.set_user_agent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36")
-    browser.open(sign_in_link)
-    browser.select_form('form[name="signIn"]')
-    browser["email"]="zephyrinkg@gmail.com"
-    browser["password"]="blah"
-    browser.launch_browser()
-    # browser.submit_selected()
+    # sign_in_link="https://www.amazon.com/ap/signin?openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&&openid.pape.max_auth_age=0"
+    driver=webdriver.Chrome()
+    driver.get("https://www.amazon.com")
+    hover_elem=driver.find_element_by_css_selector("#nav-link-accountList")
+    ActionChains(driver).move_to_element(hover_elem).perform() #hover over the sign in nav entry to get the dropdown
+    driver.find_element_by_css_selector(".nav-action-button").click() # click the link in that dropdown
+    email=driver.find_element_by_name("email")
+    password=driver.find_element_by_name("password")
+    email.send_keys("zephyrinkg@gmail.com")
+    password.send_keys("blah")
+    driver.find_element_by_css_selector("#signInSubmit").click()
 
-    # print(browser.get_current_page())
+    for i in range(0,numCaptchas):
+        captcha=driver.find_element_by_id("auth-captcha-image")
+        captcha_src=captcha.get_attribute("src")
+        urllib.request.urlretrieve(captcha_src,"captchas/captcha%i.jpg" % (i))
+        driver.find_element_by_id("auth-captcha-refresh-link").click()
