86db1e53ffe00436aedb56289089f041715c8677
jzheadley <headleyjz@vcu.edu>
2018-11-01 10:28:56 -0400

    added a script that finds duplicate captchas because they happen

---
+++ getting_captchas/bullying-amazon/findDuplicates.py
@@ -0,0 +1,31 @@
+import hashlib
+import os
+
+captcha_dir = "captchas/"
+
+hashes = []
+for file in os.listdir(captcha_dir):
+    hashes.append(
+        (file, hashlib.md5(open(captcha_dir+file, 'rb').read()).hexdigest())
+    )
+
+# print(hashes)
+
+seen = {}
+dupes = []
+dupe_hashes = []
+for x in hashes:
+    if x[1] not in seen:
+        seen[x[1]] = [x[0]]
+    else:
+        if seen[x[1]] is not None:
+            seen[x[1]].append(x[0])
+            dupes.append(x[0])
+        # seen[x[1]] += 1
+dupes.sort()
+
+# print(seen)
+for key, value in seen.items():
+    if value.__len__() > 1:
+        value.sort()
+        print(key,":",value)
--- getting_captchas/bullying-amazon/imageProcessing.py
+++ getting_captchas/bullying-amazon/imageProcessing.py
@@ -70,7 +70,7 @@ def contourImage(img, rows_to_delete, cols_to_delete):
     cols_to_delete = cols_to_delete[:10]
     for i in range(0, cols_to_delete.__len__()):
         new_img[:, cols_to_delete[i]] = 0
-    
+
     kernel = np.ones((4, 2), np.uint8)
     erosion = cv2.erode(new_img, kernel, iterations=1)
     kernel = np.ones((1, 2), np.uint8)
@@ -109,39 +109,75 @@ def contourImage(img, rows_to_delete, cols_to_delete):
     return new_img, output
 
 
-def erosionDilation(img,img_src):
-    global count, numImages,out_dir
+def union(a, b):
+    # http://answers.opencv.org/question/90455/how-to-perform-intersection-or-union-operations-on-a-rect-in-python/?answer=90504#post-id-90504
+    x = min(a[0], b[0])
+    y = min(a[1], b[1])
+    w = max(a[0]+a[2], b[0]+b[2]) - x
+    h = max(a[1]+a[3], b[1]+b[3]) - y
+    return (x, y, w, h)
+
+
+def intersection(a, b):
+    # http://answers.opencv.org/question/90455/how-to-perform-intersection-or-union-operations-on-a-rect-in-python/?answer=90504#post-id-90504
+    x = max(a[0], b[0])
+    y = max(a[1], b[1])
+    w = min(a[0]+a[2], b[0]+b[2]) - x
+    h = min(a[1]+a[3], b[1]+b[3]) - y
+    if w < 0 or h < 0:
+        return (0, 0, 0, 0)
+    return (x, y, w, h)
+
+
+def join_inner_boxes(letter_image_regions):
+    bounding_boxes = []
+    for bound_rect1 in letter_image_regions:
+        flag = False
+        for bound_rect2 in letter_image_regions:
+            if bound_rect1 == bound_rect2:
+                continue
+            bound_rect3 = intersection(bound_rect1, bound_rect2)
+            # print(bound_rect3)
+            area = bound_rect3[2]*bound_rect3[3]
+            if area > 0 and not flag:
+                print("found some overlap")
+                flag = True
+                bounding_boxes.append(union(bound_rect1, bound_rect2))
+        if not flag:
+            bounding_boxes.append(bound_rect1)
+    return bounding_boxes
+
+
+def erosionDilation(img, img_src):
+    global count, numImages, out_dir
     or_img = img
     columns = 3
     img = img[:-17, :]
     img = np.delete(img, range(1, 10), axis=0)
 
-    _,img=cv2.threshold(img,125,255,cv2.THRESH_BINARY_INV)
+    _, img = cv2.threshold(img, 125, 255, cv2.THRESH_BINARY_INV)
 
     img = cv2.medianBlur(img, 3)
     # img = cv2.bilateralFilter(img, 3, 75, 75)
-    
-    kernel=np.ones((2,3),np.uint8)
-    erosion_1=cv2.erode(img,kernel,iterations=1)
-    kernel=np.ones((3,1),np.uint8)
-    erosion_2=cv2.erode(erosion_1,kernel,iterations=1)
 
-    kernel=np.ones((2,2),np.uint8)
-    kernel[0][1]=0
-    kernel[1][0]=0
-    dilation=cv2.dilate(erosion_2,kernel,iterations=1)
-    # canny = cv2.Canny(dilation, 50, 150)
-    _, contours, _ = cv2.findContours( dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    kernel = np.ones((2, 3), np.uint8)
+    erosion_1 = cv2.erode(img, kernel, iterations=1)
+    kernel = np.ones((3, 1), np.uint8)
+    erosion_2 = cv2.erode(erosion_1, kernel, iterations=1)
 
-    _,output = cv2.threshold(dilation,125,255,cv2.THRESH_BINARY_INV)
+    kernel = np.ones((2, 2), np.uint8)
+    kernel[0][1] = 0
+    kernel[1][0] = 0
+    dilation = cv2.dilate(erosion_2, kernel, iterations=1)
+    _, contours, _ = cv2.findContours(
+        dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    _, output = cv2.threshold(dilation, 125, 255, cv2.THRESH_BINARY_INV)
 
     letter_image_regions = []
-    # cv2.drawContours(output, contours, -1, 100, 2)
-
-
 
     cv2.imwrite(out_dir+img_src, output)
-    _,output = cv2.threshold(output,125,255,cv2.THRESH_BINARY_INV)
+    _, output = cv2.threshold(output, 125, 255, cv2.THRESH_BINARY_INV)
 
     # rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 15))
     # threshed = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, rect_kernel)
@@ -159,25 +195,35 @@ def erosionDilation(img,img_src):
             continue
         else:
             letter_image_regions.append((x, y, w, h))
-            cv2.drawContours(output, contours, -1, 100, 2)
+            cv2.drawContours(output, contours, -1, 100, 3)
+    print(letter_image_regions.__len__())
+
+    bounding_boxes = join_inner_boxes(letter_image_regions)
+    print(bounding_boxes.__len__())
+
 
     letter_image_regions = sorted(letter_image_regions, key=lambda x: x[0])
     for letter_bounding_box in letter_image_regions:
-        print(letter_bounding_box)
-
+        # print(letter_bounding_box)
         # Grab the coordinates of the letter in the image
         x, y, w, h = letter_bounding_box
-        cv2.rectangle(output, (x - 2, y - 2), (x + w + 4, y + h + 4), 200, 1)
+        cv2.rectangle(dilation, (x - 2, y - 2), (x + w + 4, y + h + 4), 200, 1)
+
+    for letter_bounding_box in bounding_boxes:
+        # print(letter_bounding_box)
+        # Grab the coordinates of the letter in the image
+        x, y, w, h = letter_bounding_box
+        cv2.rectangle(output, (x - 2, y - 2), (x + w + 4, y + h + 4), 225, 1)
 
     plt.subplot(numImages, columns, count)
     plt.imshow(or_img)
     count += 1
-    # plt.subplot(numImages, columns, count)
-    # plt.imshow(erosion_2)
-    # count += 1
-    plt.subplot(5, columns, count)
-    plt.imshow(dilation)
+    plt.subplot(numImages, columns, count)
+    plt.imshow(erosion_2)
     count += 1
+    # plt.subplot(5, columns, count)
+    # plt.imshow(dilation)
+    # count += 1
     plt.subplot(numImages, columns, count)
     plt.imshow(output)
     count += 1
@@ -193,12 +239,11 @@ if __name__ == "__main__":
     row_counts = np.zeros(img.shape[0]-20).reshape(img.shape[0]-20, 1)
     column_counts = np.zeros(img.shape[1]).reshape(img.shape[1], 1)
 
-
     for img_src in captcha_image_files:
         print(img_src)
         img = cv2.imread(captcha_dir+img_src, cv2.IMREAD_GRAYSCALE)
         # deletionMethod(img,row_counts,column_counts)
-        erosionDilation(img,img_src)
+        erosionDilation(img, img_src)
 
     plt.tight_layout()
     plt.show()
