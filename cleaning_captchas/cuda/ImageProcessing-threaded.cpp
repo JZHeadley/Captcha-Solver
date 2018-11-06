470d209b4eca135328cadddf6f28664618def017
jzheadley <headleyjz@vcu.edu>
2018-11-06 10:41:48 -0500

    added the cmd line args for directories to the threaded processing

--- cleaning_captchas/cuda/ImageProcessing-threaded.cpp
+++ cleaning_captchas/cuda/ImageProcessing-threaded.cpp
@@ -6,16 +6,22 @@
 vector<string> files;
 vector<Mat> images;
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	struct timespec start, end;
 	clock_gettime(CLOCK_MONOTONIC_RAW, &start);
 	string baseDir = "/home/headleyjz/captcha_data/captchas_solved/";
 	string outDir = "/home/headleyjz/captcha_data/solution_cleaned/";
+	if (argc == 3)
+	{
+		printf("got directory arguments\n");
+		baseDir = argv[1];
+		outDir = argv[2];
+	}
+	printf("reading images from %s and writing to %s\n", baseDir.c_str(), outDir.c_str());
 	files = getFiles(baseDir);
 	images = getImages(baseDir, files);
-//	Mat* processed = (Mat*) malloc(sizeof(Mat) * images.size());
+	//	Mat* processed = (Mat*) malloc(sizeof(Mat) * images.size());
 	vector<Mat> processed;
 	Mat thresh, blur, erosion_1, erosion_2, dilation;
 	vector<int> compression_params;
@@ -36,13 +42,11 @@ int main(int argc, char* argv[])
 		dilate(erosion_2, dilation, kernel);
 		processed.push_back(dilation);
 		imwrite(outDir + files[i], dilation, compression_params);
-
 	}
 	clock_gettime(CLOCK_MONOTONIC_RAW, &end);
 	uint64_t diff = (1000000000L * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec) / 1e6;
 
-	printf("The image processing for %lu images required %llu ms CPU time.\n", images.size(), (long long unsigned int) diff);
+	printf("The image processing for %lu images required %llu ms CPU time.\n", images.size(), (long long unsigned int)diff);
 
 	return 0;
 }
-
